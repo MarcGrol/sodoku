ea5eff083218306975b659d464c2deb9c9c52657
Marc Grol <mgrol@xebia.com>
2015-11-11 12:14:40 +0100

    Added hardest solution

--- game_solve_test.go
+++ game_solve_test.go
@@ -76,3 +76,28 @@ _ _ _ 8 _ 3 _ _ _
 		}
 	}
 }
+
+func TestSolveHardestSolution(t *testing.T) {
+	gameData := `8 _ _ _ _ _ _ _ _
+_ _ 3 6 _ _ _ _ _
+_ 7 _ _ 9 _ 2 _ _
+_ 5 _ _ _ 7 _ _ _
+_ _ _ _ 4 5 7 _ _
+_ _ _ 1 _ _ _ 3 _
+_ _ 1 _ _ _ _ 6 8
+_ _ 8 5 _ _ _ 1 _
+_ 9 _ _ _ _ 4 _ _
+`
+	game, err := Load(gameData)
+	if err != nil {
+		t.Errorf("Error loading game: %s", err)
+	}
+	solutions, err := Solve(game, 10, 1)
+	if err != nil {
+		t.Errorf("Error solving game: %s", err)
+	} else {
+		if len(solutions) != 1 {
+			t.Errorf("expected: %d, actual: %d", 1, len(solutions))
+		}
+	}
+}
